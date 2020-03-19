cdab35b7c5e694a9e9767fc2ad3862a40685227b
Sodruldeen Mustapha <mustyzod@gmail.com>
2020-03-19 14:24:31 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Database: {
-        mongoURI: "mongodb+srv://test:test123@cluster0-pni2b.mongodb.net/test?retryWrites=true&w=majority",
-    },
-    Authentication: {
-        jwtSecret: "nodeprojectsecret"
-    }
-}
